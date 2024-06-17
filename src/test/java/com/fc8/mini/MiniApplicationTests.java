fa5c803ae7a88b386f24ab37ebb96755a5042d94
bom04 <gil1122@naver.com>
2024-06-17 19:18:21 +0900

    feat: project 설정

---
+++ src/test/java/com/fc8/mini/MiniApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fc8.mini;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiniApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
