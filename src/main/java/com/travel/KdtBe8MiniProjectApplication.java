f014ba211cf59f536fb54213ff4ba50ed56aa8da
Mungi Cheon <c.mungi7421@gmail.com>
2024-06-18 11:46:48 +0900

    #1 conf : 프로젝트 초기화

---
+++ src/main/java/com/travel/KdtBe8MiniProjectApplication.java
@@ -0,0 +1,13 @@
+package com.travel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KdtBe8MiniProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KdtBe8MiniProjectApplication.class, args);
+	}
+
+}
