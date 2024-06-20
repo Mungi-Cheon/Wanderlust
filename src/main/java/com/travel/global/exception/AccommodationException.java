6ffa088cb073265aff191356206974221b3e139a
fullm <fullmoon0207@naver.com>
2024-06-20 10:05:32 +0900

    refactor: protected -> public 변경

--- src/main/java/com/travel/global/exception/AccommodationException.java
+++ src/main/java/com/travel/global/exception/AccommodationException.java
@@ -1,13 +1,19 @@
 package com.travel.global.exception;
 
+import com.travel.global.exception.type.ErrorType;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.web.client.HttpStatusCodeException;
 
 public class AccommodationException extends HttpStatusCodeException {
 
-    protected AccommodationException(HttpStatusCode statusCode, String statusText) {
+    public AccommodationException(HttpStatusCode statusCode, String statusText) {
         super(statusCode, statusText);
     }
+
+    public AccommodationException(ErrorType errorType) {
+        super(HttpStatusCode.valueOf(errorType.getStatusCode()));
+    }
+
 
     @Override
     public String getMessage() {
