a5ea6c7be4ea77a5a6d20f21ccabd1cc7ce788e0
fullm <fullmoon0207@naver.com>
2024-06-20 10:06:10 +0900

    feat: 에러 코드 추가

--- src/main/java/com/travel/global/exception/type/ErrorType.java
+++ src/main/java/com/travel/global/exception/type/ErrorType.java
@@ -10,6 +10,10 @@ public enum ErrorType {
     BAD_REQUEST(400, "잘못된 요청입니다."),
     AUTH_NOT_EXIST(401, "권한이 없습니다."),
     SERVER_ERROR(500, "서버 에러입니다."),
+
+    EMPTY_ACCOMMODATION(400, "숙소 정보가 없습니다"),
+
+    INVALID_NUMBER_OF_PEOPLE(400, "잘못된 인원")
     ;
 
     private final int statusCode;
