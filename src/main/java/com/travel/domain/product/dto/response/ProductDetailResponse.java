3d2f4faea61ff6ce305ab9ed8aec52cc30f63921
Mungi Cheon <c.mungi7421@gmail.com>
2024-06-25 18:14:13 +0900

    refactor : #23 객실 도메인 리팩토링

--- src/main/java/com/travel/domain/product/dto/response/ProductDetailResponse.java
+++ src/main/java/com/travel/domain/product/dto/response/ProductDetailResponse.java
@@ -4,41 +4,53 @@ package com.travel.domain.product.dto.response;
 import com.travel.domain.product.entity.Product;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
 
 @Builder
-@Getter
 @AllArgsConstructor
-@NoArgsConstructor
 public class ProductDetailResponse {
-  private Long id;
-  private String name;
-  private String accommodationName;
-  private String description;
-  private int totalPrice;
-  private int pricePerNight;// 1박당 가격
-  private int numberOfStay; //숙박일수
-  private int standardNumber;
-  private int maximumNumber;
-  private String type;
-  private ProductImageResponse productImageResponse;
-  private ProductOptionResponse productOption; // 편의시설
 
-  public static ProductDetailResponse from(Product product, String accommodationName, int pricePerNight, int totalPrice, int numberOfStay, ProductImageResponse productImageResponse, ProductOptionResponse productOptionResponse) {
-    return ProductDetailResponse.builder()
-        .id(product.getId())
-        .name(product.getName())
-        .accommodationName(accommodationName)
-        .description(product.getDescription())
-        .pricePerNight(pricePerNight)
-        .totalPrice(totalPrice)
-        .numberOfStay(numberOfStay)
-        .standardNumber(product.getStandardNumber())
-        .maximumNumber(product.getMaximumNumber())
-        .type(product.getType())
-        .productImageResponse(productImageResponse)
-        .productOption(productOptionResponse)
-        .build();
-  }
+    private Long id;
+
+    private String name;
+
+    private String accommodationName;
+
+    private String description;
+
+    private int totalPrice;
+
+    private int pricePerNight;
+
+    private int numberOfStay;
+
+    private int standardNumber;
+
+    private int maximumNumber;
+
+    private String type;
+
+    private ProductImageResponse productImageResponse;
+
+    private ProductOptionResponse productOption;
+
+
+    public static ProductDetailResponse from(Product product, String accommodationName,
+        int pricePerNight, int totalPrice,
+        int numberOfStay, ProductImageResponse productImageResponse,
+        ProductOptionResponse productOptionResponse) {
+        return ProductDetailResponse.builder()
+            .id(product.getId())
+            .name(product.getName())
+            .accommodationName(accommodationName)
+            .description(product.getDescription())
+            .pricePerNight(pricePerNight)
+            .totalPrice(totalPrice)
+            .numberOfStay(numberOfStay)
+            .standardNumber(product.getStandardNumber())
+            .maximumNumber(product.getMaximumNumber())
+            .type(product.getType())
+            .productImageResponse(productImageResponse)
+            .productOption(productOptionResponse)
+            .build();
+    }
 }
--- src/main/java/com/travel/domain/product/entity/ProductOption.java
+++ src/main/java/com/travel/domain/product/entity/ProductOption.java
@@ -11,14 +11,8 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToOne;
-import jakarta.persistence.Table;
-import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
 
-@AllArgsConstructor
-@NoArgsConstructor
 @Getter
 @Entity
 public class ProductOption {
@@ -38,15 +32,25 @@ public class ProductOption {
     @JoinColumn(name = "accommodation_id")
     private Accommodation accommodation;
 
-    private boolean hasBath;
-    private boolean hasAirCondition;
-    private boolean hasTv;
-    private boolean hasPc;
-    private boolean hasWifi;
-    private boolean hasCable;
-    private boolean hasRefrigerator;
-    private boolean hasSofa;
-    private boolean canCook;
-    private boolean hasTable;
-    private boolean hasHairdryer;
+    private Boolean hasBath;
+
+    private Boolean hasAirCondition;
+
+    private Boolean hasTv;
+
+    private Boolean hasPc;
+
+    private Boolean hasWifi;
+
+    private Boolean hasCable;
+
+    private Boolean hasRefrigerator;
+
+    private Boolean hasSofa;
+
+    private Boolean canCook;
+
+    private Boolean hasTable;
+
+    private Boolean hasHairdryer;
 }
