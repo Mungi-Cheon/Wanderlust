740f085b808f0d2a1d0a4d7767563d3b235b16a9
Mungi Cheon <c.mungi7421@gmail.com>
2024-06-25 18:09:19 +0900

    refactor : #21 예약도메인 리팩토링

--- src/main/java/com/travel/domain/reservations/controller/ReservationController.java
+++ src/main/java/com/travel/domain/reservations/controller/ReservationController.java
@@ -10,38 +10,40 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/api/reservation")
 public class ReservationController {
 
     private final ReservationService reservationService;
 
-    @GetMapping("/reservations")
+    @GetMapping("/history")
     public ResponseEntity<ReservationHistoryListResponse> getReservationHistories() {
         ReservationHistoryListResponse response = reservationService.getReservationHistories(
-            "test@gmail.com");
+            "c.mungi7421@gmail.com");
         return ResponseEntity.ok(response);
     }
 
-    @PostMapping("/reservation")
+    @PostMapping()
     public ResponseEntity<ReservationResponse> reservation(
         @Valid @RequestBody ReservationRequest reservationRequest) {
-        ReservationResponse response = reservationService.saveReservation(reservationRequest,
-            "sneor4807@gmail.com");
+        ReservationResponse response = reservationService.createReservation(reservationRequest,
+            "c.mungi7421@gmail.com");
         return ResponseEntity.ok(response);
     }
 
-//    @GetMapping("/reservations")
+//    @GetMapping("/history2")
 //    public ResponseEntity<ReservationHistoryListResponse> getReservations(
 //        Authentication authentication) {
-//        ReservationHistoryListResponse response = reservationService.getReservation(
+//        ReservationHistoryListResponse response = reservationService.getReservationHistories(
 //            authentication.getName());
 //        return ResponseEntity.ok(response);
 //    }
 //
-//    @PostMapping("/reservation")
+//    @PostMapping("/reservation2")
 //    public ResponseEntity<ReservationResponse> reservation(
 //        @Valid @RequestBody ReservationRequest reservationRequest, Authentication authentication) {
 //        ReservationResponse response = reservationService.saveReservation(reservationRequest,
--- src/main/java/com/travel/domain/reservations/dto/response/ReservationResponse.java
+++ src/main/java/com/travel/domain/reservations/dto/response/ReservationResponse.java
@@ -1,29 +1,30 @@
 package com.travel.domain.reservations.dto.response;
 
-import com.travel.domain.reservations.entity.Reservations;
+import com.travel.domain.reservations.entity.Reservation;
 import java.time.LocalDate;
 import java.time.temporal.ChronoUnit;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
 
 @Getter
-@Setter
 @AllArgsConstructor
-@NoArgsConstructor
 @Builder
 public class ReservationResponse {
 
     private Long id;
+
     private Integer personNumber;
+
     private Integer totalPrice;
+
     private Integer price;
+
     private LocalDate checkInDate;
+
     private LocalDate checkOutDate;
 
-    public static ReservationResponse from(Reservations entity) {
+    public static ReservationResponse from(Reservation entity) {
         return ReservationResponse.builder()
             .id(entity.getId())
             .personNumber(entity.getPersonNumber())
