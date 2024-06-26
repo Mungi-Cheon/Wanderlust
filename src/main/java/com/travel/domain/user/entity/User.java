810ba8247ca148be81146067734071f63385186d
hajunghoon <haline963@gmail.com>
2024-06-26 14:53:06 +0900

    fix: user 도메인 수정

---
+++ src/main/java/com/travel/domain/user/entity/User.java
@@ -0,0 +1,42 @@
+package com.travel.domain.user.entity;
+
+import com.travel.domain.user.dto.request.SignupRequest;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    private String username;
+
+    private String password;
+
+    private String email;
+
+    @Builder
+    public User(String email, String username, String password) {
+        this.email = email;
+        this.username = username;
+        this.password = password;
+    }
+
+    public static User from(
+        SignupRequest request,String password) {
+        return User.builder()
+            .email(request.getEmail())
+            .username(request.getUsername())
+            .password(password)
+            .build();
+    }
+}
