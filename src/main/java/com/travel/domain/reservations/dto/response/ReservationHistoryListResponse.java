f9cc48c02e010cb6cee0c25052be2ba87a392fe1
Mungi Cheon <c.mungi7421@gmail.com>
2024-06-27 20:24:56 +0900

    test : #28 예약도메인 controller, service, repository 테스트 케이스 추가

--- src/main/java/com/travel/domain/reservations/dto/response/ReservationHistoryListResponse.java
+++ src/main/java/com/travel/domain/reservations/dto/response/ReservationHistoryListResponse.java
@@ -3,9 +3,11 @@ package com.travel.domain.reservations.dto.response;
 import java.util.List;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
+import lombok.Getter;
 
 @AllArgsConstructor
 @Builder
+@Getter
 public class ReservationHistoryListResponse {
 
     private List<ReservationHistoryResponse> reservationHistoryList;
