91b5bb26f5cde2bf393537e4efaf360b7e71f987
Mungi Cheon <c.mungi7421@gmail.com>
2024-07-03 20:04:20 +0900

    hotfix : test누락

--- src/main/java/com/travel/domain/accommodation/repository/AccommodationRepository.java
+++ src/main/java/com/travel/domain/accommodation/repository/AccommodationRepository.java
@@ -1,11 +1,9 @@
 package com.travel.domain.accommodation.repository;
 
 import com.travel.domain.accommodation.entity.Accommodation;
-import jakarta.persistence.LockModeType;
 import java.util.List;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Lock;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
@@ -24,10 +22,9 @@ public interface AccommodationRepository extends JpaRepository<Accommodation, Lo
         "WHERE a.category = :category")
     List<Accommodation> findAllAccommodationsByCategory(@Param("category") String category);
 
-    @Lock(LockModeType.PESSIMISTIC_WRITE)
     @Query("SELECT a FROM Accommodation a " +
         "LEFT JOIN FETCH a.images " +
         "LEFT JOIN FETCH a.options " +
         "WHERE a.id = :id")
-    Optional<Accommodation> findByIdJoinAndImagesOptionsWithPessimisticLock(@Param("id") Long id);
+    Optional<Accommodation> findByIdJoinImagesAndOptions(@Param("id") Long id);
 }
