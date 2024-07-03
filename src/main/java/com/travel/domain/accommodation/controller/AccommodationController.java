d4838375b4c2f42a83a48b1008d8b892ac2bbe65
Mungi Cheon <c.mungi7421@gmail.com>
2024-07-03 19:55:41 +0900

    hotfix : accommodation 캐싱, 페이지네이션 로직 롤백

--- src/main/java/com/travel/domain/accommodation/controller/AccommodationController.java
+++ src/main/java/com/travel/domain/accommodation/controller/AccommodationController.java
@@ -2,24 +2,20 @@ package com.travel.domain.accommodation.controller;
 
 import com.travel.domain.accommodation.dto.response.AccommodationResponse;
 import com.travel.domain.accommodation.service.AccommodationService;
-import com.travel.domain.reservations.dto.response.ReservationHistoryListResponse;
 import com.travel.global.util.DateValidationUtil;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import java.time.LocalDate;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import java.time.LocalDate;
 
 @Tag(name = "Accommodation API", description = "숙소 API")
 @RestController
@@ -29,6 +25,29 @@ public class AccommodationController {
 
     private final AccommodationService accommodationService;
 
+//    @Operation(summary = "숙소 조회", description = "조건에 맞게 숙소를 조회합니다.")
+//    @ApiResponse(content = @Content(mediaType = "application/json",
+//        schema = @Schema(implementation = AccommodationResponse.class)))
+//    @GetMapping
+//    public ResponseEntity<Page<AccommodationResponse>> getAvailableAccommodations(
+//        @RequestParam(required = false) String category,
+//        @RequestParam(required = false) LocalDate checkInDate,
+//        @RequestParam(required = false) LocalDate checkOutDate,
+//        @RequestParam(defaultValue = "2") int personNumber,
+//        @RequestParam(defaultValue = "0") int page,
+//        @RequestParam(defaultValue = "8") int size)  {
+//
+//        checkInDate = DateValidationUtil.checkInDate(checkInDate);
+//        checkOutDate = DateValidationUtil.checkOutDate(checkInDate, checkOutDate);
+//
+//        Pageable pageable = PageRequest.of(page, size, Sort.by(Sort.Direction.ASC,"id"));
+//
+//        Page<AccommodationResponse> responses = accommodationService
+//            .getAvailableAccommodations(category, checkInDate, checkOutDate, personNumber, pageable)
+//            ;
+//        return ResponseEntity.ok(responses);
+//    }
+
     @Operation(summary = "숙소 조회", description = "조건에 맞게 숙소를 조회합니다.")
     @ApiResponse(content = @Content(mediaType = "application/json",
         schema = @Schema(implementation = AccommodationResponse.class)))
@@ -36,19 +55,14 @@ public class AccommodationController {
     public ResponseEntity<Page<AccommodationResponse>> getAvailableAccommodations(
         @RequestParam(required = false) String category,
         @RequestParam(required = false) LocalDate checkInDate,
-        @RequestParam(required = false) LocalDate checkOutDate,
-        @RequestParam(defaultValue = "2") int personNumber,
-        @RequestParam(defaultValue = "0") int page,
-        @RequestParam(defaultValue = "8") int size)  {
+        @RequestParam(required = false) LocalDate checkOutDate) {
 
         checkInDate = DateValidationUtil.checkInDate(checkInDate);
         checkOutDate = DateValidationUtil.checkOutDate(checkInDate, checkOutDate);
 
-        Pageable pageable = PageRequest.of(page, size, Sort.by(Sort.Direction.ASC,"id"));
-
         Page<AccommodationResponse> responses = accommodationService
-            .getAvailableAccommodations(category, checkInDate, checkOutDate, personNumber, pageable)
-            ;
+            .getAvailableAccommodations(category, checkInDate, checkOutDate, personNumber,
+                pageable);
         return ResponseEntity.ok(responses);
     }
 }
