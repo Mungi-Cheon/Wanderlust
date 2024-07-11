456992eb35b84968aa753b094f3f27eb8188c9f6
Mungi Cheon <c.mungi7421@gmail.com>
2024-07-11 22:04:09 +0900

    refactor : 카테고리 관리 수정
    String -> enum으로 관리

--- src/main/java/com/travel/domain/accommodation/controller/AccommodationController.java
+++ src/main/java/com/travel/domain/accommodation/controller/AccommodationController.java
@@ -1,5 +1,6 @@
 package com.travel.domain.accommodation.controller;
 
+import com.travel.domain.accommodation.category.Category;
 import com.travel.domain.accommodation.dto.response.AccommodationResponse;
 import com.travel.domain.accommodation.service.AccommodationService;
 import com.travel.global.util.DateValidationUtil;
@@ -8,14 +9,14 @@ import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import java.time.LocalDate;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import java.time.LocalDate;
-import java.util.List;
 
 @Tag(name = "Accommodation API", description = "숙소 API")
 @RestController
@@ -30,13 +31,15 @@ public class AccommodationController {
         schema = @Schema(implementation = AccommodationResponse.class)))
     @GetMapping
     public ResponseEntity<List<AccommodationResponse>> getAvailableAccommodations(
-        @RequestParam(required = false) String category,
+        @RequestParam(required = false) Integer categoryId,
         @RequestParam(required = false) LocalDate checkInDate,
         @RequestParam(required = false) LocalDate checkOutDate,
         @RequestParam(defaultValue = "2") int personNumber) {
 
         checkInDate = DateValidationUtil.checkInDate(checkInDate);
         checkOutDate = DateValidationUtil.checkOutDate(checkInDate, checkOutDate);
+        
+        String category = Category.fromId(categoryId);
 
         List<AccommodationResponse> responses = accommodationService
             .getAvailableAccommodations(category, checkInDate, checkOutDate, personNumber);
