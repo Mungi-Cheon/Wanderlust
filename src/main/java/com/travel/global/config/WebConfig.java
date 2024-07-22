908d36bba983f86335c66c13e8cdb94247c5cdd9
Mungi Cheon <c.mungi7421@gmail.com>
2024-07-22 13:57:53 +0900

    refactor : #98 CORS설정

--- src/main/java/com/travel/global/config/WebConfig.java
+++ src/main/java/com/travel/global/config/WebConfig.java
@@ -6,6 +6,7 @@ import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -46,5 +47,16 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
         resolvers.add(tokenMemberIdResolver);
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+            .allowedOrigins("http://localhost:3000", "https://wanderlust-mini4.netlify.app")
+            .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS")
+            .allowCredentials(true)
+            .maxAge(1728000L)
+            .allowedHeaders("*")
+            .exposedHeaders("*");
     }
 }
