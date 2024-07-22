1dbe38b96d4e7402e7686c1c2c06dbb71d16adc1
Mungi Cheon <c.mungi7421@gmail.com>
2024-07-23 00:04:28 +0900

    refactor : #98 CORS설정

--- src/main/java/com/travel/global/config/WebConfig.java
+++ src/main/java/com/travel/global/config/WebConfig.java
@@ -52,10 +52,9 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-            .allowedOrigins("http://localhost:3000", "https://wanderlust-mini4.netlify.app",
-                "http://localhost")
+            .allowedOrigins("*")
             .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS")
-            .allowCredentials(true)
+            .allowCredentials(false)
             .maxAge(1728000L)
             .allowedHeaders("*")
             .exposedHeaders("*");
