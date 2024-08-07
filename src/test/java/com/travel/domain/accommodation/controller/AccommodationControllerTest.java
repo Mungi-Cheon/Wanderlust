839bf64533a25e0eafedd55d251139436ae7a495
Mungi Cheon <c.mungi7421@gmail.com>
2024-08-07 13:34:37 +0900

    test : 테스트 수정

--- src/test/java/com/travel/domain/accommodation/controller/AccommodationControllerTest.java
+++ src/test/java/com/travel/domain/accommodation/controller/AccommodationControllerTest.java
@@ -12,7 +12,6 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 import com.travel.domain.accommodation.dto.response.AccommodationResponse;
 import com.travel.domain.accommodation.service.AccommodationService;
-import com.travel.domain.config.TestConfig;
 import java.time.LocalDate;
 import java.util.List;
 import org.junit.jupiter.api.DisplayName;
@@ -24,13 +23,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.context.annotation.Import;
 import org.springframework.http.MediaType;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 
 @SpringBootTest
-@Import(TestConfig.class)
 @ExtendWith(MockitoExtension.class)
 @AutoConfigureMockMvc
 public class AccommodationControllerTest {
